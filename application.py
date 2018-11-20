0e71c97bfe69206b5a25551e6290b8795203ea70
Pranav Ganorkar <pranavg189@gmail.com>
2018-11-20 19:14:05 +0530

    Add code to initialize the database model

--- application.py
+++ application.py
@@ -1,7 +1,17 @@
 from flask import Flask, render_template, request
-import base64
+from models import *
 
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///' + os.path.join(basedir, 'urlinfo.db')
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+app.secret_key = "super secret key"
+
+db.init_app(app)
 
 @app.route('/', methods=["GET", "POST"])
 def index():
@@ -15,3 +25,11 @@ def index():
     else:
         """Display UI to capture an image"""
         return render_template("capture.html")
+
+"""Run this block of code to create the initial tables for the model"""
+def main():
+    db.create_all()
+
+if __name__ == "__main__":
+    with app.app_context():
+        main()
