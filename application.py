3cbfc165b699066a19620eaea8ad05447ead7329
Pranav Ganorkar <pranavg189@gmail.com>
2018-11-20 18:42:10 +0530

    Store capture image to local filesystem

--- application.py
+++ application.py
@@ -1,7 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import base64
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def index():
-    return render_template("capture.html")
+    if request.method == "POST":
+        image_data = request.values.get("imageBase64")
+
+        with open('image.png', 'wb') as image_file:
+            image_file.write(base64.b64decode(image_data))
+    else:
+        """Display UI to capture an image"""
+        return render_template("capture.html")
